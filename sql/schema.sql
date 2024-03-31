64429b86ec322f77fd5ad88aec1cb69c47a7d7cf
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-03-31 15:53:04 +0900

    refactor: DB 스키마 변경

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,7 +10,9 @@ CREATE TABLE IF NOT EXISTS chess_game (
 CREATE TABLE IF NOT EXISTS movement (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     chess_game_id BIGINT NOT NULL,
-    source VARCHAR(8) NOT NULL,
-    target VARCHAR(8) NOT NULL,
+    source_file VARCHAR(8) NOT NULL,
+    source_rank VARCHAR(8) NOT NULL,
+    target_file VARCHAR(8) NOT NULL,
+    target_rank VARCHAR(8) NOT NULL,
     FOREIGN KEY (chess_game_id) REFERENCES chess_game (id)
 );
