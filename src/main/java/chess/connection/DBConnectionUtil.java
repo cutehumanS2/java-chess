ab9cc547765c76c2e3680311531388fa31e093a7
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-04-01 14:28:30 +0900

    refactor: DB 접속 정보를 DBConnectionUtil에서 관리하도록 변경

--- src/main/java/chess/connection/DBConnectionUtil.java
+++ src/main/java/chess/connection/DBConnectionUtil.java
@@ -6,9 +6,16 @@ import java.sql.SQLException;
 
 public class DBConnectionUtil {
 
+    private static final String SERVER = "localhost:3306";
+    private static final String DATABASE = "chess";
+    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION;
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "110423";
+
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(ConnectionConst.URL, ConnectionConst.USERNAME, ConnectionConst.PASSWORD);
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (final SQLException e) {
             throw new IllegalStateException("DB 연결 오류:" + e.getMessage());
         }
