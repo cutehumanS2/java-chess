1bae96d8a728d8db5f07a145e586c9692fd4f553
cutehumanS2 <cherrycoke@sookmyung.ac.kr>
2024-04-03 14:06:28 +0900

    chore: 테스트 DB 생성 및 접속 테스트

--- docker/db/mysql/init/init.sql
+++ docker/db/mysql/init/init.sql
@@ -1,13 +1,11 @@
 CREATE DATABASE IF NOT EXISTS chess DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-USE chess;
-
-CREATE TABLE IF NOT EXISTS chess_game (
+CREATE TABLE IF NOT EXISTS chess.chess_game (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     turn VARCHAR(8) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS movement (
+CREATE TABLE IF NOT EXISTS chess.movement (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     chess_game_id BIGINT NOT NULL,
     source_file VARCHAR(8) NOT NULL,
@@ -17,3 +15,12 @@ CREATE TABLE IF NOT EXISTS movement (
     FOREIGN KEY (chess_game_id) REFERENCES chess_game (id),
     UNIQUE KEY unique_movement (source_file, source_rank, target_file, target_rank)
 );
+
+
+CREATE DATABASE IF NOT EXISTS chess_test DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE chess_test.chess_game LIKE chess.chess_game;
+CREATE TABLE chess_test.movement LIKE chess.movement;
+
+
+
--- src/test/java/chess/connection/TestDBConnectionUtil.java
+++ src/test/java/chess/connection/TestDBConnectionUtil.java
@@ -4,16 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnectionUtil {
+public class TestDBConnectionUtil {
 
     private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "110423";
     private static final String SERVER = "localhost:13306";
-    private static final String DATABASE = "chess";
+    private static final String DATABASE = "chess_test";
     private static final String URL = "jdbc:mysql://" + SERVER + "/" + DATABASE + OPTION;
 
-    private DBConnectionUtil() {
+    private TestDBConnectionUtil() {
     }
 
     public static Connection getConnection() {
